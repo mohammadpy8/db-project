8f3fd00ac10645c48c6889d01ebcbf88a37a13c6
mohammadpy8 <mohammadpy8@gmail.com>
2023-12-24 08:54:16 +0330

    mainChange

--- src/module/changeCrypto/index.tsx
+++ src/module/changeCrypto/index.tsx
@@ -1,12 +1,13 @@
-import { FC } from 'react'
+import { FC } from "react";
 
 const ChangeCrypto: FC = () => {
   return (
     <div className="mt-12 mr-8">
-      <div className="w-[500px] h-[500px] change-crypto rounded-full relative"></div>
-      <div className="w-[400px] h-[410px] absolute bg-white rounded-lg shadow-lg top-[165px] left-[230px]"></div>
+      <div className="w-[500px] h-[500px] change-crypto rounded-full circleCahnge relative">
+        <div className="w-[400px] h-[410px]  bg-white rounded-lg shadow-lg absolute top-12 left-12"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChangeCrypto
+export default ChangeCrypto;
