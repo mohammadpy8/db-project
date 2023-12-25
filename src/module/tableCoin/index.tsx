20afdd529b29366994759e969b5772c8a9554759
mohammadpy8 <mohammadpy8@gmail.com>
2023-12-25 22:27:06 +0330

    table

--- src/module/tableCoin/index.tsx
+++ src/module/tableCoin/index.tsx
@@ -1,61 +1,112 @@
-import { FC } from "react";
-import styles from "./Table.module.css";
-import crypto from "../../types/cryptoType/CryptoType";
+import { FC } from 'react'
+import crypto from '../../types/cryptoType/CryptoType'
+import numberConvertToPersian from '../../shared/numberConvertToPersian'
+import { FiBarChart2 } from 'react-icons/fi'
 
 interface table {
-  isLoading: boolean;
-  currency: string;
-  coins: crypto[];
+  isLoading: boolean
+  currency: string
+  coins: crypto[]
 }
 
 const TableCoin: FC<table> = ({ isLoading, coins, currency }) => {
   const changeCurrency = (price: string) => {
-    if (price === "usd") {
-      return "$";
-    } else if (price === "eur") {
-      return "#";
+    if (price === 'usd') {
+      return '$'
+    } else if (price === 'eur') {
+      return '#'
     } else {
-      return "*";
+      return '*'
     }
-  };
+  }
+
+  const randomPhoto = (priceCoin: number) => {
+    if (priceCoin > 0 && priceCoin < 3) {
+      return 'https://kifpool.me/ui_v2/charts/green/green-1.svg'
+    } else if (priceCoin >= 3 && priceCoin < 5) {
+      return 'https://kifpool.me/ui_v2/charts/green/green-10.svg'
+    } else if (priceCoin >= 5) {
+      return 'https://kifpool.me/ui_v2/charts/green/green-9.svg'
+    } else if (priceCoin < 0) {
+      return 'https://kifpool.me/ui_v2/charts/red/red-2.svg'
+    } else if (priceCoin <= -1 && priceCoin <= -3) {
+      return 'https://kifpool.me/ui_v2/charts/red/red-3.svg'
+    } else if (priceCoin < -4) {
+      return 'https://kifpool.me/ui_v2/charts/red/red-2.svg'
+    }
+  }
+
+  const colorRandom = (index: number) => {
+    if (index % 2 === 0) {
+      return '#fff'
+    } else if (index % 2 === 1) {
+      return '#f4f6ff;'
+    }
+  }
   return (
-    <div className={styles.container}>
+    <div className="mt-8 mb-8 rounded-2xl relative z-0 border-[1px] border-[#f0f3ff]">
       {isLoading ? (
         <h1>loading..</h1>
       ) : (
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>Coin</th>
-              <th>Name</th>
-              <th>Price</th>
-              <th>24h</th>
-              <th>Total Volume</th>
+        <table className="w-full rounded-xl m-auto border-collapse border-spacing-0 overflow-hidden font-Yek-SemiBold">
+          <thead className="rounded-2xl h-16 bg-[#f0f3ff] font-Yek-Bold text-gray-700">
+            <tr className="TH">
+              <th></th>
+              <th>ارز</th>
+              <th>قیمت(دلار)</th>
+              <th>تغییرات</th>
+              <th>قیمت اصلی(دلار)</th>
+              <th>نمودار</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            {coins.map((coin) => (
-              <tr>
+            {coins.map((coin, index) => (
+              <tr
+                className="TD hover:bg-gray-100 hover:scale-105 transition-all duration-500"
+                style={{
+                  backgroundColor: colorRandom(index),
+                }}
+              >
                 <td>
-                  <div className={styles.symbol}>
-                    <img src={coin.image} alt={coin.id} />
-                    <span>{coin.symbol.toUpperCase()}</span>
+                  <div className="flex flex-col items-center gap-y-2">
+                    <img src={coin.image} alt={coin.id} className="w-8 h-8" />
+                    <span className="text-[15px] font-Yek-Bold">
+                      {coin.symbol.toUpperCase()}
+                    </span>
                   </div>
                 </td>
                 <td>{coin.name}</td>
                 <td>
                   {changeCurrency(currency)}
-                  {coin.current_price.toLocaleString()}
+                  {numberConvertToPersian(coin.current_price.toLocaleString())}
                 </td>
                 <td
-                  className={coin.price_change_percentage_24h > 0 ? styles.success : styles.error}
+                  className={
+                    coin.price_change_percentage_24h > 0
+                      ? 'text-green-500'
+                      : 'text-red-600'
+                  }
                 >
-                  {coin.price_change_percentage_24h.toFixed(2)}%
+                  {numberConvertToPersian(
+                    coin.price_change_percentage_24h.toFixed(2),
+                  )}
+                  %
                 </td>
-                <td>${coin.total_volume.toLocaleString()}</td>
                 <td>
-                  {/* <img src={price_change > 0 ? chartUp : chartDown} alt={id} /> */}
+                  ${numberConvertToPersian(coin.total_volume.toLocaleString())}
+                </td>
+                <td>
+                  <img
+                    src={randomPhoto(coin.price_change_percentage_24h)}
+                    alt={coin.name}
+                    className="w-28"
+                  />
+                </td>
+                <td>
+                  <button className="border-2 p-2 rounded-lg">
+                    <FiBarChart2 size={30} color="#3051f8" />
+                  </button>
                 </td>
               </tr>
             ))}
@@ -63,7 +114,7 @@ const TableCoin: FC<table> = ({ isLoading, coins, currency }) => {
         </table>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default TableCoin;
+export default TableCoin
--- src/template/priceCrypto/index.tsx
+++ src/template/priceCrypto/index.tsx
@@ -1,10 +1,11 @@
-import { FC } from "react";
-import { FiBarChart2 } from "react-icons/fi";
-import ShowCoin from "../showCoin";
+import { FC } from 'react'
+import { FiBarChart2 } from 'react-icons/fi'
+import ShowCoin from '../showCoin'
+import { TbClipboardList } from 'react-icons/tb'
 
 const PriceCrypto: FC = () => {
   return (
-    <div className="mt-60">
+    <div className="mt-60 mb-24">
       <div className="flex items-center gap-x-2">
         <div>
           <FiBarChart2 size={30} color="#3051f8" />
@@ -15,11 +16,17 @@ const PriceCrypto: FC = () => {
           </h1>
         </div>
       </div>
-      <div className="">
+      <div>
         <ShowCoin />
       </div>
+      <div className="flex justify-center">
+        <button className="flex items-center gap-x-2 font-Yek-SemiBold text-primary-200 border-2 p-2 rounded-lg hover:opacity-70 transition-all duration-300">
+          <TbClipboardList color="#193df3" size={30} />
+          <span>لیست تمام ارزها</span>
+        </button>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default PriceCrypto;
+export default PriceCrypto
